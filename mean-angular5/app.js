b99463bfc4d43a1f64891bcf60fce12a6b1e13b1
Refael-Moblin <Refael@MOBLIN.com>
2018-05-26 21:42:42 +0300

    add site url

--- mean-angular5/app.js
+++ mean-angular5/app.js
@@ -1,3 +1,5 @@
+//https://www.djamware.com/post/5a0673c880aca7739224ee21/mean-stack-angular-5-crud-web-application-example
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
